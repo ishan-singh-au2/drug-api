918a2977e4d134591974a6f51f3e6abb6a6fd668
Ishan <its.ishan.kumar@gmail.com>
2019-11-09 06:18:42 +0530

    get-single-drug-class

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ app.get('/api/get-drug-classes', (req, res) => {
     res.json({ data: db.medications })
 })
 
+app.get('/api/get-single-drug-class/:drugClass', (req, res) => {
+    res.json({ data: db.medications[0][req.params.drugClass] })
+})
+
+
 const listener = app.listen(8080, _ => console.log(`server running on ${listener.address().port}`))
