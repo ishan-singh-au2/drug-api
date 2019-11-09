56f37d978b19fe7237d48a5ad23a0697edb2c846
Ishan <its.ishan.kumar@gmail.com>
2019-11-09 06:19:04 +0530

    add-drug-class

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ app.get('/api/get-single-drug-class/:drugClass', (req, res) => {
     res.json({ data: db.medications[0][req.params.drugClass] })
 })
 
+app.post('/api/add-drug-class', (req, res) => {
+    let flag = false
+    if (!db.medications[0].hasOwnProperty(req.body.drugClass)) {
+        db.medications[0][req.body.drugClass] = []
+        flag = true
+    }
+    res.json({ success: flag })
+})
+
 
 const listener = app.listen(8080, _ => console.log(`server running on ${listener.address().port}`))
